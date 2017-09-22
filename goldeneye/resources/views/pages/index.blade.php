d5fdbb2a56f23fd2023ea8b975b00e053bfaff59
Mark Cummins <markrc@hawaii.edu>
2017-09-22 00:05:55 -1000

    Deleted unused code

--- goldeneye/resources/views/pages/index.blade.php
+++ goldeneye/resources/views/pages/index.blade.php
@@ -12,22 +12,6 @@
 
 @section ('content')
 
-    <div id="app">
-        <ais-index
-                app-id="latency"
-                api-key="3d9875e51fbd20c7754e65422f7ce5e1"
-                index-name="bestbuy"
-        >
-            <ais-search-box></ais-search-box>
-            <ais-results>
-                <template scope="{ result }">
-                    <h2>
-                        <ais-highlight :result="result" attribute-name="name"></ais-highlight>
-                    </h2>
-                </template>
-            </ais-results>
-        </ais-index>
-    </div>
     ------
     Home Page Stuff Here
     -------
